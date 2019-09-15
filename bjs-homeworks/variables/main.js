1e221f631273fa993504c0d3b4f351f702b06c46
Evgenya Sharapova <eva.sharapova1@mail.ru>
2019-09-15 20:06:29 +0300

    first commit variables

---
+++ bjs-homeworks/variables/main.js
@@ -0,0 +1,15 @@
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let averageMark = (algebra + geography + physics) / 3;
+console.log(averageMark);
+
+let name = "Евгения";
+let message = `Привет, мир! Меня зовут ${name}.`;
+console.log(message);
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
